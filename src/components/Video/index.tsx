b40a7d7957059bc9c4f69976a82522ca5527529e
rickturner2001 <stayup.dev@gmail.com>
2024-12-21 00:06:34 +0100

    traduzione

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -13,8 +13,8 @@ const Video = () => {
     <section className="relative z-10 py-16 md:py-20 lg:py-28">
       <div className="container">
         <SectionTitle
-          title="We are ready to help"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          title="Riabilitazione e Fisioterapia"
+          paragraph="Dai un'occhiata a quello che facciamo a Kinesis per saperne di più su di noi."
           center
           mb="80px"
         />
